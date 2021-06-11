c54644edcdaad5b50f20f34e19969f60def2fac4
DJPIE1337 <djpie1337@gmail.com>
2021-06-11 22:56:07 +0200

    dockerfile env var

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 4000,
-    APIURL: process.env.APIURL || "http://localhost:3000/api/v1/",
+    APIURL: "http://"+process.env.APIURL || "http://localhost:3000/api/v1/",
     ISPROD: process.env.ISPROD || false
 }
