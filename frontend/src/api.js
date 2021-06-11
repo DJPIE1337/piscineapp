e299837c167e7279fcf0cf059348528a79718be2
DJPIE1337 <djpie1337@gmail.com>
2021-06-12 01:26:08 +0200

    baseurl

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const {APIURL} = process.env
+const APIURL = process.env.NODE_ENV === 'production' ? 'http://piscineapp.zbeb.eu/api/v1/' : 'http://localhost:3000/api/v1/';
 
 const API = axios.create({
   withCredentials: true,
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-const {ISPROD} = process.env
 
-if (ISPROD) {console.log = console.warn = console.error = () => {};}
+if (process.env.NODE_ENV === 'production') {console.log = console.warn = console.error = () => {};}
 
 ReactDOM.render(<App />, document.getElementById('root'));
